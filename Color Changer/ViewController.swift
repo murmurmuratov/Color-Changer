32be1b82b997bcefdfa5a7523b3ab4ab2c0c891f
Alexander Muratov <MuratovAlexanderVasilievich@ya.ru>
2022-01-20 13:01:51 +0300

    Added Several Optimizing Functions

--- Color Changer/ViewController.swift
+++ Color Changer/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var changedColorView: UIView!
+    
     @IBOutlet var redValueLabel: UILabel!
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLabel: UILabel!
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
+    
     @IBOutlet var informationBGView: UIView!
     
     override func viewDidLoad() {
@@ -28,40 +31,40 @@ class ViewController: UIViewController {
         informationBGView.layer.cornerRadius = 20
         informationBGView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         
-        changedColorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1)
-        // Do any additional setup after loading the view.
+        setColor()
+        setValue(for: redValueLabel, greenValueLabel, blueValueLabel)
     }
 
     @IBAction func redSliderAction(_ sender: Any) {
         redValueLabel.text = String(Int(redSlider.value))
-        changedColorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value)/255,
-            green: CGFloat(greenSlider.value)/255,
-            blue: CGFloat(blueSlider.value)/255,
-            alpha: 1)
+        setColor()
     }
     
     @IBAction func greenSliderAction(_ sender: Any) {
         greenValueLabel.text = String(Int(greenSlider.value))
-        changedColorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value)/255,
-            green: CGFloat(greenSlider.value)/255,
-            blue: CGFloat(blueSlider.value)/255,
-            alpha: 1)
+        setColor()
     }
     
     @IBAction func blueSliderAction(_ sender: Any) {
         blueValueLabel.text = String(Int(blueSlider.value))
-        changedColorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value)/255,
-            green: CGFloat(greenSlider.value)/255,
-            blue: CGFloat(blueSlider.value)/255,
-            alpha: 1)
+        setColor()
     }
     
+    private func setColor() {
+        changedColorView.backgroundColor = UIColor( red: CGFloat(redSlider.value) / 255,
+                                                    green: CGFloat(greenSlider.value) / 255,
+                                                    blue: CGFloat(blueSlider.value) / 255,
+                                                    alpha: 1)
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0: redValueLabel.text = String(Int(redSlider.value))
+            case 1: greenValueLabel.text = String(Int(greenSlider.value))
+            case 2: blueValueLabel.text = String(Int(blueSlider.value))
+            default: break
+            }
+        }
+    }
 }
-
